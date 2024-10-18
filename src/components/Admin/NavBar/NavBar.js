cf551397142aefc90d3f93afd24c15b1017d805c
Thanh Danh <danhhoang0145@gmail.com>
2024-10-19 03:12:48 +0700

    admin

--- src/components/Admin/NavBar/NavBar.js
+++ src/components/Admin/NavBar/NavBar.js
@@ -1,38 +1,92 @@
 import React, { useState } from "react";
-
 import "./index.css";
-import { TbCategory } from "react-icons/tb";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai"; // Mũi tên qua trái và phải
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import { HiHome } from "react-icons/hi";
+import { MdManageAccounts } from "react-icons/md";
+import { HiArrowSmallDown, HiArrowSmallUp } from "react-icons/hi2";
+import { IoDocumentsOutline } from "react-icons/io5";
+import { IoReloadCircle } from "react-icons/io5";
+import { IoCheckmarkDoneCircle } from "react-icons/io5";
+import { FaUser } from "react-icons/fa6";
+import { BiCategory } from "react-icons/bi";
+import { GrLogout } from "react-icons/gr";
 
 function NavBar() {
-  const [isExpanded, setIsExpanded] = useState(true); // Khởi tạo state để kiểm tra trạng thái mở rộng của Navbar
+  const [isExpanded, setIsExpanded] = useState(true);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleNavBar = () => {
     setIsExpanded(!isExpanded);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
     <div className={`containerNavBar ${isExpanded ? "expanded" : "collapsed"}`}>
       <div className={`navbar`}>
         <div className="navbar-content">
-          <div className="itemAdmin">
+          <div className={`itemAdmin ${isExpanded ? "expanded" : "collapsed"}`}>
             <HiHome />
-            {isExpanded && <span>Trang chủ</span>}
+            {isExpanded && <span className="titleAdminManege">Trang chủ</span>}
           </div>
-          <div className="itemAdmin">
-            <TbCategory />
-            {isExpanded && <span>Quản lí thể loại</span>}
+          <div className={`itemAdmin ${isExpanded ? "expanded" : "collapsed"}`}>
+            <FaUser />
+            {isExpanded && (
+              <span className="titleAdminManege">Quản lí người dùng</span>
+            )}
+          </div>
+          <div
+            className={`itemAdmin ${isExpanded ? "expanded" : "collapsed"}`}
+            onClick={toggleDropdown} // Toggle khi click vào
+          >
+            <MdManageAccounts />
+            {isExpanded && (
+              <span className="titleAdminManege">
+                <span>Quản lí tài liệu</span>{" "}
+                <div className="iconDownUp">
+                  {isDropdownOpen ? <HiArrowSmallUp /> : <HiArrowSmallDown />}
+                </div>
+              </span>
+            )}
+          </div>
+          {isExpanded && isDropdownOpen && (
+            <div className="dropdown">
+              <div className="dropdown-item">
+                <IoDocumentsOutline />
+                <span>Tất cả tài liệu</span>
+              </div>
+              <div className="dropdown-item">
+                <IoReloadCircle />
+                <span>Tài liệu chờ duyệt</span>
+              </div>
+              <div className="dropdown-item">
+                <IoCheckmarkDoneCircle />
+                <span>Tài liệu đã duyệt</span>
+              </div>
+            </div>
+          )}
+          <div className={`itemAdmin ${isExpanded ? "expanded" : "collapsed"}`}>
+            <BiCategory />
+            {isExpanded && (
+              <span className="titleAdminManege">Quản lí thể loại</span>
+            )}
+          </div>
+          <div className={`itemAdmin ${isExpanded ? "expanded" : "collapsed"}`}>
+            <GrLogout />
+            {isExpanded && <span className="titleAdminManege">Đăng xuất</span>}
           </div>
         </div>
       </div>
-      <button onClick={toggleNavBar}>
-        {isExpanded ? (
-          <AiOutlineLeft /> // Hiện mũi tên trái khi mở rộng
-        ) : (
-          <AiOutlineRight /> // Hiện mũi tên phải khi thu nhỏ
-        )}
-      </button>
+      <div className="btn">
+        <button
+          onClick={toggleNavBar}
+          className={`btnLeftRight ${isExpanded ? "expanded" : "collapsed"}`}
+        >
+          {isExpanded ? <AiOutlineLeft /> : <AiOutlineRight />}
+        </button>
+      </div>
     </div>
   );
 }
--- src/layouts/admin/HomeAdmin.js
+++ src/layouts/admin/HomeAdmin.js
@@ -1,6 +1,54 @@
+// import React from "react";
+// import NavBar from "../../components/Admin/NavBar/NavBar";
+// import "./css/index.css";
+// import avatar from "../../assets/iconAva.png";
+// import { FaUser } from "react-icons/fa6";
+// import { FiSearch } from "react-icons/fi";
+
+// function HomeAdmin() {
+//   return (
+//     <div className="containerHomeAdmin">
+//       <div className="leftHomeAdmin">
+//         <NavBar />
+//       </div>
+//       <div className="rightHomeAdmin">
+//         <div className="containerHeaderAdmin">
+//           <div className="avatarAdmin">
+//             <img src={avatar} alt="avatar" className="avatar" />
+//           </div>
+//           <div className="fullNameAdmin">Admin</div>
+//         </div>
+//         <div className="containerInfoDocument">
+//           <div className="containerSearchAdmin">
+//             <span className="titleInfoAdmin">
+//               <FaUser className="iconUser" />
+//               <span className="titleInfo">Thông tin người dùng</span>
+//             </span>
+//             <div className="searchDocumentAdmin">
+//               <input
+//                 type="text"
+//                 placeholder="Tìm kiếm tài liệu"
+//                 className="inputSearchAdmin"
+//               />
+//               <FiSearch className="searchIcon" />
+//             </div>
+//           </div>
+
+//           <div className="infoDocumentAdmin"></div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default HomeAdmin;
+
 import React from "react";
 import NavBar from "../../components/Admin/NavBar/NavBar";
 import "./css/index.css";
+import avatar from "../../assets/iconAva.png";
+import { FaUser } from "react-icons/fa6";
+import { FiSearch } from "react-icons/fi";
 
 function HomeAdmin() {
   return (
@@ -9,8 +57,65 @@ function HomeAdmin() {
         <NavBar />
       </div>
       <div className="rightHomeAdmin">
-        <div className="titleAdmin">admin</div>
-        <div>s</div>
+        <div className="containerHeaderAdmin">
+          <div className="avatarAdmin">
+            <img src={avatar} alt="avatar" className="avatar" />
+          </div>
+          <div className="fullNameAdmin">Admin</div>
+        </div>
+        <div className="containerInfoDocument">
+          <div className="containerSearchAdmin">
+            <span className="titleInfoAdmin">
+              <FaUser className="iconUser" />
+              <span className="titleInfo">Thông tin người dùng</span>
+            </span>
+            <div className="searchDocumentAdmin">
+              <input
+                type="text"
+                placeholder="Tìm kiếm tài liệu"
+                className="inputSearchAdmin"
+              />
+              <FiSearch className="searchIcon" />
+            </div>
+          </div>
+
+          <div className="infoDocumentAdmin">
+            <table className="documentTable">
+              <thead>
+                <tr>
+                  <th>STT</th>
+                  <th>Hình ảnh</th>
+                  <th>Tên hiển thị</th>
+                  <th>Họ tên</th>
+                  <th>Mã số SV</th>
+                  <th>Email</th>
+                  <th>Địa chỉ</th>
+                  <th>Quyền</th>
+                  <th>Tài liệu sở hữu</th>
+                  <th>Khóa tài khoản</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>
+                    <img src={avatar} alt="User" className="userImage" />
+                  </td>
+                  <td>John Doe</td>
+                  <td>Nguyễn Văn A</td>
+                  <td>123456</td>
+                  <td>john.doe@example.com</td>
+                  <td>Hà Nội</td>
+                  <td>Admin</td>
+                  <td>10 tài liệu</td>
+                  <td>
+                    <button className="btnLock">Khóa</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
